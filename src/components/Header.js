38770f61c71f9c57b962f95a913ebde6ead57244
Spencer Stock <spencerstock@gmail.com>
2019-11-08 08:48:31 -0700

    Initial changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,12 @@ import PropTypes from 'prop-types'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon fa-cubes"></span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Spencer Stock</h1>
+                <p>Android Developer finishing Lambda School's 9 month intensive program, Android track.</p>
             </div>
         </div>
         <nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -115,55 +115,38 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
-                className="icon fa-twitter"
+                href="https://www.linkedin.com/in/spencer-stock-33a257175/"
+                className="icon fa-linkedin"
               >
-                <span className="label">Twitter</span>
+                <span className="label">Linkedin</span>
               </a>
             </li>
+            
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://facebook.com/spencerstock" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/spencerstock"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
               </a>
             </li>
+            <li>
+              <a
+                href="mailto:spencerstock@gmail.com'"
+                className="icon fa-envelope"
+              >
+                <span className="label">E-mail</span>
+              </a>
+            </li>
+            
           </ul>
           {close}
         </article>
